513b3fb6b6d6d64844ab07bac2acea8c8a6b4f60
7745678 <142322939+7745678@users.noreply.github.com>
2026-08-01 22:48:52 +0300

    Create database_init.py
    from app.database import engine
    from app.models import *
    
    from app.database import Base
    
    
    def create_tables():
        Base.metadata.create_all(bind=engine)
    
    
    if __name__ == "__main__":
        create_tables()
        print("Database tables created successfully")

---
+++ backend/backend/app/database_init.py
@@ -0,0 +1,13 @@
+from app.database import engine
+from app.models import *
+
+from app.database import Base
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    create_tables()
+    print("Database tables created successfully")
